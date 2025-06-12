55d4f7e1a9069448981f56a8ed23a75a651e2530
Rajeswari1705 <Rajeswari.Nahak@cognizant.com>
2025-06-12 21:16:47 +0530

    Added API Gateway and Discovery Server modules

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.usermanagementservice;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient //Enables Eureka client functionality
-public class UsermanagementServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UsermanagementServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-//add a comment
+
 }
