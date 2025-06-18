24fa72ef16147cf5af05445d2f2fd7782e1d7c4a
Mounika2406329 <Naraboyini.Mounika@cognizant.com>
2025-06-18 08:47:33 +0530

    updated

--- travel-package-management-service/travel-package-management2/src/main/java/com/example/dto/OfferDTO.java
+++ travel-package-management-service/travel-package-management2/src/main/java/com/example/dto/OfferDTO.java
@@ -1,5 +1,12 @@
 package com.example.dto;
 
+import lombok.Data;
+
+@Data
 public class OfferDTO {
+	private String couponCode;
+	private String description;
+	private int discountPercentage;
+	private boolean active;
 
 }
