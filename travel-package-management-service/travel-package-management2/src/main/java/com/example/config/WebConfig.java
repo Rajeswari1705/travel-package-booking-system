651ae09d9ec6bda634a02db7b230d72064e4d737
Mounika2406329 <Naraboyini.Mounika@cognizant.com>
2025-06-26 22:53:16 +0530

    updated

--- travel-package-management-service/travel-package-management2/src/main/java/com/example/config/WebConfig.java
+++ travel-package-management-service/travel-package-management2/src/main/java/com/example/config/WebConfig.java
@@ -8,6 +8,8 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
   }
 }
